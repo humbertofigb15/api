44174ddf429ddda830bc0debdefecdf2b9305cda
Humberto Figueroa Bouttier <a01722877@tec.mx>
2025-04-28 10:30:02 -0600

    final

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -36,6 +36,52 @@ export const postUsers = (req,res) => {
     });
 };
 
-export const putUser = (req,res) => {};
-export const deleteUsers = (req,res) => {};
-export const login = (req,res) => {};
+export const putUser = (req,res) => {
+    const {name, username, password, age} = req.body;
+    pool.execute("update users set name = ?, username=?,password=?,age=? where id = ?", 
+        [name, username, password, age, req.params.id],
+        (error,results) =>
+        {
+        if (error) {
+            res.status(500).json({ msg: error, users: [] });
+            return;
+        }
+        res.status(200).json({ msg: "OK", users: results });
+    });
+};
+
+export const deleteUsers = (req,res) => {
+    pool.execute("delete from users where id = ?", 
+        [req.params.id],
+        (error,results) =>
+        {
+        if (error) {
+            res.status(500).json({ msg: error, users: [] });
+            return;
+        }
+        res.status(200).json({ msg: "OK", users: results });
+    });
+};
+
+export const login = (req,res) => {
+    const {username,password} = req.body;
+    pool.execute(
+        "select * from users where username = ?",
+        [username],
+        (error,results) =>
+            {
+            if (error) {
+                res.status(500).json({ msg: error, users: [] });
+                return;
+            }
+            if (results.length < 1){
+            res.status(401).json({ isLogin: false, msg: "Credenciales Invalidas", user: {} });
+            return;
+            }
+            if(results[0].password === password){
+             res.status(200).json({ isLogin: true, msg: "Entro", user: results[0] });
+            }else{
+                res.status(401).json({ isLogin: false, msg: "Credenciales Invalidas", user: {} });
+            }
+        });
+};
